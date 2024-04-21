480d9eb419802a676b6bedd1f7638ac1c02afb07
Ryoichi MATSUMOTO <iyo.rotomus@gmail.com>
2024-04-21 11:53:45 +0900

    code refactoring

--- app/app.py
+++ app/app.py
@@ -1,26 +1,45 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import pandas as pd
 import json
 import sys
+import secrets
 
 app = Flask(__name__)
+app.secret_key = secrets.token_urlsafe(16)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    print(f'Request: {request.method}', file=sys.stderr)
+    print(f'Request: {request}', file=sys.stderr)
+    
+    data = None if 'data' not in session else session['data']
+    params = None if 'params' not in session else session['params']
+    
     if request.method == 'POST':
-        csv_file = request.files['file']
-        graph_type = request.form.get('graph_type')
-        print(f'{graph_type}, {csv_file}', file=sys.stderr)
-        df = pd.read_csv(csv_file)
-        data = {
-            'x': df['x'].tolist(),
-            'y': df['y'].tolist(),
-            'graph_type': graph_type
-        }
-        data = json.dumps(data).replace("'", '"')
-        print(f'{data}', file=sys.stderr)
-        return render_template('index.html', data=data)
+        print(f'Request Form: {request.form}', file=sys.stderr)
+        print(f'  - post_type: {request.form.get("post_type")}', file=sys.stderr)
+        if (request.form.get('post_type') == 'Upload'):
+            if 'params' in session:
+                del session['params']
+                params = None
+            
+            csv_file = request.files['file']
+            
+            df = pd.read_csv(csv_file)
+            data = {key: df[key].tolist() for key in df.columns}
+            data = json.dumps(data).replace("'", '"')
+            session['data'] = data
+            print(f'{data}', file=sys.stderr)
+            
+        elif (request.form.get('post_type') == 'Apply'):
+            graph_type = request.form.get('graph_type')
+            
+            params = {
+                'graph_type': graph_type
+            }
+            params = json.dumps(params).replace("'", '"')
+            session['params'] = params
+        
+        return render_template('index.html', data=data, params=params)
     return render_template('index.html')
 
 if __name__ == '__main__':
